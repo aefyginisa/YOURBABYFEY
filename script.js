da3a5ca69757c508ac62f068524ecadd434f0163
Fey <fanne.araza@gmail.com>
2025-08-02 17:41:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const envelope = document.getElementById('envelope');
+const flap = document.getElementById('flap');
 const paper = document.getElementById('paper');
 const buttons = document.getElementById('buttons');
 
 function showPaper() {
-  paper.classList.add('show');
+  flap.classList.add('flap-open');
   setTimeout(() => {
+    paper.classList.add('show');
     envelope.style.opacity = '0';
     buttons.style.opacity = '0';
     buttons.style.pointerEvents = 'none';
-  }, 1000);
+  }, 600);
 }
 
 function hidePaper() {
   paper.classList.remove('show');
+  flap.classList.remove('flap-open');
   envelope.style.opacity = '1';
   buttons.style.opacity = '1';
   buttons.style.pointerEvents = 'auto';
